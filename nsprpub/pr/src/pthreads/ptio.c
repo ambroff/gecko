e0bdace685b52da452d0bd97f839657d6142e467
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2020-11-06 15:36:34 -0800

    Apply nspr-4.26.patchset from haikuports
    Notes from that patchset:
    
    * NSPR 4.19 migration by Kacper Kasper
    * Old patchset wouldn't apply anymore.
    * Platform code by korli.
    * pr/include/obsolete/prototypes.h by pulkomandy.
    * Fix build of libpurple.
    * pr/include/prtypes.h by Kacper Kasper.
      Fix build of NSS. NSS uses XP_BEOS and defs for BeOS fail on GCC7.

--- nsprpub/pr/src/pthreads/ptio.c
+++ nsprpub/pr/src/pthreads/ptio.c
@@ -28,7 +28,7 @@
 #include <sys/uio.h>
 #include <sys/file.h>
 #include <sys/ioctl.h>
-#if defined(DARWIN)
+#if defined(DARWIN) || defined(__HAIKU__)
 #include <sys/utsname.h> /* for uname */
 #endif
 #if defined(SOLARIS) || defined(UNIXWARE)
@@ -183,7 +183,7 @@ static PRBool _pr_ipv6_v6only_on_by_default;
     || defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
     || defined(FREEBSD) || defined(NETBSD) || defined(OPENBSD) \
     || defined(BSDI) || defined(NTO) || defined(DARWIN) \
-    || defined(UNIXWARE) || defined(RISCOS)
+    || defined(UNIXWARE) || defined(RISCOS)  || defined(__HAIKU__)
 #define _PRSelectFdSetArg_t fd_set *
 #else
 #error "Cannot determine architecture"
@@ -3477,7 +3477,7 @@ static PRIOMethods _pr_socketpollfd_methods = {
     || defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
     || defined(AIX) || defined(FREEBSD) || defined(NETBSD) \
     || defined(OPENBSD) || defined(BSDI) || defined(NTO) \
-    || defined(DARWIN) || defined(UNIXWARE) || defined(RISCOS)
+    || defined(DARWIN) || defined(UNIXWARE) || defined(RISCOS) || defined(__HAIKU__)
 #define _PR_FCNTL_FLAGS O_NONBLOCK
 #else
 #error "Can't determine architecture"
